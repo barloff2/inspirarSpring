740e5d8f6f878641c808d33224394e3c1980554b
Stiven <barloff2@gmail.com>
2023-11-25 18:31:59 -0500

    First commit

---
+++ src/main/java/co/com/inspirar/InspirarApplication.java
@@ -0,0 +1,13 @@
+package co.com.inspirar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspirarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspirarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/inspirar/InspirarApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.inspirar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspirarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
